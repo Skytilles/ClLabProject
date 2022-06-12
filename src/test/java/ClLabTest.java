e35622ad1b63edfcf15097f62cd78b4391979776
Amy <amy.pollom@gmail.com>
2022-06-12 14:38:35 -0700

    updated testing code

--- src/test/java/ClLabTest.java
+++ src/test/java/ClLabTest.java
@@ -1,8 +1,7 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ClLabTest {
     ClLab m;
@@ -17,22 +16,22 @@ public class ClLabTest {
     }
     @Test
     public void getTheString(){
-      String a = "Test String";
-      String actual = m.setString(a);
-        assertTrue(a == actual.getString(), true, "Test String");
+      String a = m.setString("Test String");
+      String actual = m.getString();
+      assertTrue(a == actual, "Test String");
     }
     @Test
     public void noCapitalLetters(){
-        String a = "abcde";
+        String a = m.setString("abcde");
         String actual = m.detectCapitalUse();
-        assertTrue();
+        assertFalse(a == actual, "String does not have a capital letter");
     }
 
     @Test
     public void yesCapitalLetters(){
-        String a = "abCde";
+        String a = m.setString("Abcde");
         String actual = m.detectCapitalUse();
-
+        assertTrue(a == actual, "String has a capital letter");
     }
 
 
